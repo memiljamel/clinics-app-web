dd46bedaa9c22ccc89db8b84de24b642b47b0251
Emil Jamel Mahmuda <memiljamel@gmail.com>
2023-10-01 14:05:15 +0700

    feat: add new fields in users table, update user factory and create user seeder

---
+++ database/migrations/2023_09_30_112056_add_new_fields_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\Role;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_number', 14)->nullable();
+            $table->text('photo')->nullable();
+            $table->enum('role', [Role::Administrator->value, Role::Patient->value])->default(Role::Patient->value);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone_number');
+            $table->dropColumn('photo');
+            $table->dropColumn('role');
+        });
+    }
+};
