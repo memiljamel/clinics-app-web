8de84c192c7f8ea436ae4e07c80dfd1bae9d5430
Emil Jamel Mahmuda <memiljamel@gmail.com>
2023-10-16 10:59:42 +0700

    refactor: testing:LoginTest.php

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
@@ -32,20 +31,19 @@ class LoginTest extends TestCase
         $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
             'email' => 'email@domain.com',
             'password' => 'password',
+            'remember' => true,
         ]);
         $response->assertSessionHasNoErrors();
         $response->assertRedirect('dashboard');
         $response->assertStatus(302);
 
-        $this->assertEquals('email@domain.com', $user->email);
-        $this->assertTrue(Hash::check('password', $user->password));
         $this->assertAuthenticatedAs($user);
     }
 
     /** @test */
     public function asserting_that_administrator_failed_to_authenticate_with_blank_data(): void
     {
-        $user = User::factory()->administrator()->create([
+        User::factory()->administrator()->create([
             'email' => 'email@domain.com',
             'password' => 'password',
         ]);
@@ -53,21 +51,23 @@ class LoginTest extends TestCase
         $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
             'email' => null,
             'password' => null,
+            'remember' => true,
         ]);
+        $response->assertRedirect(route('login.index'));
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(['remember']);
         $response->assertSessionHasErrors([
             'email' => __('validation.required', ['attribute' => 'email']),
             'password' => __('validation.required', ['attribute' => 'password']),
         ]);
 
-        $this->assertNotEquals(null, $user->email);
-        $this->assertNotEquals(null, $user->email);
         $this->assertGuest();
     }
 
     /** @test */
     public function asserting_that_administrator_failed_to_authenticate_with_invalid_email(): void
     {
-        $user = User::factory()->administrator()->create([
+        User::factory()->administrator()->create([
             'email' => 'email@domain.com',
             'password' => 'password',
         ]);
@@ -75,19 +75,45 @@ class LoginTest extends TestCase
         $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
             'email' => 'invalid_email',
             'password' => 'password',
+            'remember' => true,
         ]);
+        $response->assertRedirect(route('login.index'));
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(['password', 'remember']);
         $response->assertSessionHasErrors([
             'email' => __('validation.email', ['attribute' => 'email']),
         ]);
 
-        $this->assertNotEquals('invalid_email', $user->email);
+        $this->assertGuest();
+    }
+
+    /** @test */
+    public function asserting_that_administrator_failed_to_authenticate_with_email_does_not_exists(): void
+    {
+        User::factory()->administrator()->create([
+            'email' => 'email@domain.com',
+            'password' => 'password',
+        ]);
+
+        $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
+            'email' => 'email@doesnot.exists',
+            'password' => 'password',
+            'remember' => true,
+        ]);
+        $response->assertRedirect(route('login.index'));
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(['password', 'remember']);
+        $response->assertSessionHasErrors([
+            'email' => __('validation.exists', ['attribute' => 'email']),
+        ]);
+
         $this->assertGuest();
     }
 
     /** @test */
     public function asserting_that_administrator_failed_to_authenticate_with_invalid_password(): void
     {
-        $user = User::factory()->administrator()->create([
+        User::factory()->administrator()->create([
             'email' => 'email@domain.com',
             'password' => 'password',
         ]);
@@ -95,33 +121,84 @@ class LoginTest extends TestCase
         $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
             'email' => 'email@domain.com',
             'password' => 'invalid_password',
+            'remember' => true,
         ]);
+        $response->assertRedirect(route('login.index'));
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(['password', 'remember']);
         $response->assertSessionHasErrors([
             'email' => __('auth.failed', ['attribute' => 'email']),
         ]);
 
-        $this->assertNotEquals('invalid_password', $user->password);
         $this->assertGuest();
     }
 
     /** @test */
-    public function asserting_that_administrator_failed_to_authenticate_with_invalid_email_and_password(): void
+    public function asserting_that_administrator_failed_to_authenticate_with_password_too_short(): void
     {
-        $user = User::factory()->administrator()->create([
+        User::factory()->administrator()->create([
             'email' => 'email@domain.com',
             'password' => 'password',
         ]);
 
         $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
-            'email' => 'invalid_email',
-            'password' => 'invalid_password',
+            'email' => 'email@domain.com',
+            'password' => 'secret',
+            'remember' => true,
         ]);
+        $response->assertRedirect(route('login.index'));
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(['email', 'remember']);
         $response->assertSessionHasErrors([
-            'email' => __('validation.email', ['attribute' => 'email']),
+            'password' => __('validation.min.string', ['attribute' => 'password', 'min' => 8]),
         ]);
 
-        $this->assertNotEquals('invalid_email', $user->email);
-        $this->assertNotEquals('invalid_password', $user->password);
+        $this->assertGuest();
+    }
+
+    /** @test */
+    public function asserting_that_administrator_failed_to_authenticate_with_password_too_long(): void
+    {
+        User::factory()->administrator()->create([
+            'email' => 'email@domain.com',
+            'password' => 'password',
+        ]);
+
+        $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
+            'email' => 'email@domain.com',
+            'password' => str_repeat('secret', 71),
+            'remember' => true,
+        ]);
+        $response->assertRedirect(route('login.index'));
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(['email', 'remember']);
+        $response->assertSessionHasErrors([
+            'password' => __('validation.max.string', ['attribute' => 'password', 'max' => 70]),
+        ]);
+
+        $this->assertGuest();
+    }
+
+    /** @test */
+    public function asserting_that_administrator_failed_to_authenticate_with_remember_field_not_a_boolean_value(): void
+    {
+        User::factory()->administrator()->create([
+            'email' => 'email@domain.com',
+            'password' => 'password',
+        ]);
+
+        $response = $this->from(route('login.index'))->post(route('login.authenticate'), [
+            'email' => 'email@domain.com',
+            'password' => 'password',
+            'remember' => 'not_a_boolean_value',
+        ]);
+        $response->assertRedirect(route('login.index'));
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(['email', 'password']);
+        $response->assertSessionHasErrors([
+            'remember' => __('validation.boolean', ['attribute' => 'remember']),
+        ]);
+
         $this->assertGuest();
     }
 
